cd9514be70d18516f05a09c44d49d045ef46a357
Devree Lewis <devree.lewis@gmail.com>
2019-12-19 17:34:47 -0500

    added nested functions- need help with arrow notation

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 'use strict';
 
-let countryCapital = "replace";
-let response5Json = "replace";
-let response4Json = "replace";
-let response3Json = "replace";
-let response2Json = "replace";
-let response1Json = "replace"; 
+function watchForm(){
+  $('form').submit(event => {
+    event.preventDefault();
+    const country = $('#js-country').val();
+    getCountryFromApi(country);
+  });
+}
+
+$(watchForm);
 
 //runs 7th
-function displayTimeResults(response5Json){//, response2Json, countryCapital){
+displayTimeResults() = ((response5Json,countryCapital,response2Json) => {
   console.log(response5Json);
-  // console.log(response2Json);
-  // console.log(response1Json);
+  console.log(countryCapital);
+  console.log(response2Json);
   let actualTime = response5Json.datetime;
   let actualTimeString = actualTime.toString();
   let actualTimeShort = actualTimeString.substring(11,16);
   console.log(actualTimeShort);
 $('#timeresults').empty();
 $('#timeresults').append(
-    `<p>It's actually ${actualTimeShort}</p>` 
-    // in` ${countryCapital}. Maybe you need 
-    //     ${response2Json.data.translations[1].translatedText} (Good afternoon!) or 
-    //     ${response2Json.data.translations[2].translatedText} (Good evening!) ?</p>`
+    `<p>It's actually ${actualTimeShort}in
+         ${countryCapital}. Maybe you need 
+         ${response2Json.data.translations[1].translatedText} (Good afternoon!) or 
+         ${response2Json.data.translations[2].translatedText} (Good evening!) ?</p>`
   );
 $('#timeresults').removeClass('hidden');
-}
-
+})
 
 //runs 6th
-function timeZone2(response4Json){
+timeZone2() = (response4Json => {
   console.log(response4Json);
-  return new Promise(()=>{
   let timeZone = response4Json.timeZoneId;
   console.log(timeZone);
   fetch("https://worldtimeapi.org/api/timezone/"+timeZone)
@@ -40,57 +41,33 @@ function timeZone2(response4Json){
     }
     throw new Error(res5.statusText);
   })
-  .then(response5Json => {console.log(response5Json);
-    displayTimeResults(response5Json)})//, response2Json, countryCapital)})
+  .then(response5Json => console.log(response5Json))//, response2Json, countryCapital)})
   .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  });
+  })
 })
-}
-
 
 //runs 5th
-function timeZone1(response3Json){
+timeZone1() = (response3Json => {
   console.log(response3Json);
-
-  return new Promise(() =>{
   let lat=response3Json.results[0].geometry.location.lat;
   let long=response3Json.results[0].geometry.location.lng;
   const timeStamp = Date.now();
   const timeStampString = timeStamp.toString();
   let timeStampShort= timeStampString.substring(0, (timeStampString.length)-3);
-  fetch("https://maps.googleapis.com/maps/api/timezone/json?location="+lat+","+long+"&timestamp="+timeStampShort+"&key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks")
-  .then(res4 => {
-    if (res4.ok) {
-      return res4.json();
-    }
-    throw new Error(res4.statusText);
-  })
-  .then(response4Json => timeZone2(response4Json))
-  .catch(err => {
-    $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  })})
-}
+  request("https://maps.googleapis.com/maps/api/timezone/json?location="+lat+","+long+"&timestamp="+timeStampShort+"&key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks")
+  }
+)
+
 
 //runs fourth
-function geoCoding(response1Json){
-  let countryCapital=response1Json[0].capital;
-  let countryCode=response1Json[0].alpha2Code;
+function geoCoding(translateData){
+  let countryCapital=translateData[0].capital;
+  let countryCode=translateData[0].alpha2Code;
 
-  fetch("https://maps.googleapis.com/maps/api/geocode/json?address="+countryCapital+"&components=country:"+countryCode+"&key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks")
-  .then(res3 => {
-   if (res3.ok) {
-     return res3.json();
-   }
-   throw new Error(res3.statusText);
- })
- .then(response3Json => {console.log(response3Json);
-  timeZone1(response3Json)})
- .catch(err => {
-   $('#js-error-message').text(`Something went wrong: ${err.message}`);
- })
+  request("https://maps.googleapis.com/maps/api/geocode/json?address="+countryCapital+"&components=country:"+countryCode+"&key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks")
 }
- 
+
 //runs third, works
 function displayTranslationResults(response2Json)
 { console.log(response2Json);
@@ -105,10 +82,10 @@ $('#results').removeClass('hidden');
 }
 
 //runs second, triggers displayTranslationResults() and geoCoding() (also takes response1Json as parameter)
-function googleTranslate(response1Json) {
-   console.log(response1Json);
+function googleTranslate(translateData) {
+   console.log(translateData);
 
-   let language = response1Json[0].languages[0];
+   let language = translateData[0].languages[0];
    console.log(language);
 
    const data1 = {
@@ -125,24 +102,14 @@ function googleTranslate(response1Json) {
     body: JSON.stringify(data1)
    } 
      
- fetch("https://translation.googleapis.com/language/translate/v2?key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks", options1)
-   .then(res2 => {
-    if (res2.ok) {
-      return res2.json();
-    }
-    throw new Error(res2.statusText);
-  })
+ request("https://translation.googleapis.com/language/translate/v2?key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks", options1)
   .then(response2Json => displayTranslationResults(response2Json))
-  .catch(err => {
-    $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  });
-  geoCoding(response1Json);
 }
 
 debugger
 
-//runs first, sends data to googleTranslate
-function getResults(country) {
+//runs first, sends data to googleTranslate and geoCoding
+function getCountryFromApi(country) {
 
 const url = "https://restcountries-v1.p.rapidapi.com/name/"+country;
 
@@ -156,27 +123,25 @@ console.log(url);
 
 debugger
 
-  fetch(url, options)
+  request(url, options).then((translateData) => {
+    googleTranslate(translateData);
+    geoCoding(translateData)
+      .then(timeZone1())
+      .then(timeZone2())
+      .then(displayTimeResults());
+  })
+}
+
+function request(url, options) {
+  return fetch(url, options)
     .then(res1 => {
       if (res1.ok) {
         return res1.json();
       }
       throw new Error(res1.statusText);
     })
-    .then(response1Json => googleTranslate(response1Json))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
 }
 
-debugger
-
-function watchForm(){
-  $('form').submit(event => {
-    event.preventDefault();
-    const country = $('#js-country').val();
-    getResults(country);
-  });
-}
-
-$(watchForm);
