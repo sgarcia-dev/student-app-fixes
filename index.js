19bcdf4271800d546a3d001ffcafba2096dd0490
Devree Lewis <devree.lewis@gmail.com>
2019-12-19 14:44:22 -0500

    commit from old computer

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ $('#timeresults').append(
         ${response2Json.data.translations[2].translatedText} (Good evening!) ?</p>`
   );
 $('#timeresults').removeClass('hidden');
-
 }
+
 
 //runs 6th
 function timeZone2(response4Json){
   console.log(response4Json);
+  return new Promise(()=>{
   let timeZone = response4Json.timeZoneId;
   console.log(timeZone);
   fetch("https://worldtimeapi.org/api/timezone/"+timeZone)
@@ -38,22 +39,24 @@ function timeZone2(response4Json){
     }
     throw new Error(res5.statusText);
   })
-  .then(response5Json => {console.log(response5Json)})
+  .then(response5Json => {console.log(response5Json);
+    displayTimeResults(response5Json, response2Json, countryCapital)})
   .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
   });
-  console.log(response5Json);
+  console.log(response5Json);})
 }
 
 //runs 5th
 function timeZone1(response3Json){
   console.log(response3Json);
+
+  return new Promise(() =>{
   let lat=response3Json.results[0].geometry.location.lat;
   let long=response3Json.results[0].geometry.location.lng;
   const timeStamp = Date.now();
   const timeStampString = timeStamp.toString();
   let timeStampShort= timeStampString.substring(0, (timeStampString.length)-3);
-  console.log(timeStampShort);
   fetch("https://maps.googleapis.com/maps/api/timezone/json?location="+lat+","+long+"&timestamp="+timeStampShort+"&key=AIzaSyDumOtzsZBkWdtNzTDcdsVLKYJ6yJUtkks")
   .then(res4 => {
     if (res4.ok) {
@@ -61,10 +64,10 @@ function timeZone1(response3Json){
     }
     throw new Error(res4.statusText);
   })
-  .then(response4Json => console.log(response4Json))
+  .then(response4Json => timeZone2(response4Json))
   .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
-  });
+  })})
 }
 
 //runs fourth
@@ -79,12 +82,11 @@ function geoCoding(response1Json){
    }
    throw new Error(res3.statusText);
  })
- .then(response3Json => {
-  console.log(response3Json)  
+ .then(response3Json => {console.log(response3Json);
   timeZone1(response3Json)})
- .then(response4Json => timeZone2(response4Json))
- .then(response5Json => displayTimeZone(response5Json, response2Json, countryCapital)) 
- .catch(err => {
+ .then(response5Json => displayTimeResults(response5Json, response2Json, countryCapital)) 
+ 
+  .catch(err => {
    $('#js-error-message').text(`Something went wrong: ${err.message}`);
  })
 }
@@ -131,10 +133,10 @@ function googleTranslate(response1Json) {
     throw new Error(res2.statusText);
   })
   .then(response2Json => displayTranslationResults(response2Json))
-  .then(geoCoding(response1Json))
   .catch(err => {
     $('#js-error-message').text(`Something went wrong: ${err.message}`);
   });
+  geoCoding(response1Json);
 }
 
 debugger
